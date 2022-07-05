f36c047e1e3d6eea5de749a0a8f65ebd8bb51df0
Israel Gutiérrez <jisg383@gmail.com>
2022-07-05 13:23:32 -0500

    changes

---
+++ spring-cloud-stream-rabbitmq-consumer-example/src/main/java/com/jisg/rabbitmq/SpringCloudStreamRabbitmqConsumerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jisg.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamRabbitmqConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamRabbitmqConsumerExampleApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-stream-rabbitmq-consumer-example/src/test/java/com/jisg/rabbitmq/SpringCloudStreamRabbitmqConsumerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisg.rabbitmq;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamRabbitmqConsumerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
